fe44881481b31ac62674aee9d1b08a90b858995b
James Dagnall <james.t.dagnall@gmail.com>
2025-09-07 11:21:32 +1000

    formatting

--- flake.nix
+++ flake.nix
@@ -1,135 +1,137 @@
 {
-    description = "Neovim configuration, compatible with nix and non-nix systems. Thanks Loki.";
+	description = "Neovim configuration, compatible with nix and non-nix systems. Thanks Loki.";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        nixCats.url = "github:BirdeeHub/nixCats-nvim";
-        # neovim-nightly-overlay = {
-        #   url = "github:nix-community/neovim-nightly-overlay";
-        # };
-        # see :help nixCats.flake.inputs
-        # If you want your plugin to be loaded by the standard overlay,
-        # i.e. if it wasnt on nixpkgs, but doesnt have an extra build step.
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+		nixCats.url = "github:BirdeeHub/nixCats-nvim";
+		# neovim-nightly-overlay = {
+		#   url = "github:nix-community/neovim-nightly-overlay";
+		# };
+		# see :help nixCats.flake.inputs
+		# If you want your plugin to be loaded by the standard overlay,
+		# i.e. if it wasnt on nixpkgs, but doesnt have an extra build step.
 
-        # because this input which does not have a nixpkg is named
-        # "plugins-<name>" it will be overlayed on
-        # nixpkgs.neovimPlugins by the nixCats.utils.standardPluginOverlay
-        # function below
-        plugins-harpoon-lualine = {
-            url = "github:letieu/harpoon-lualine";
-            flake = false;
-        };
-    };
+		# because this input which does not have a nixpkg is named
+		# "plugins-<name>" it will be overlayed on
+		# nixpkgs.neovimPlugins by the nixCats.utils.standardPluginOverlay
+		# function below
+		plugins-harpoon-lualine = {
+			url = "github:letieu/harpoon-lualine";
+			flake = false;
+		};
+	};
 
-    # see :help nixCats.flake.outputs
-    outputs =
-        {
-            # self,
-            nixpkgs,
-            nixCats,
-            ...
-        }@inputs:
-        let
-            inherit (nixCats) utils;
-            luaPath = ./.; # lua configuration is in this same directory
-            forEachSystem = utils.eachSystem nixpkgs.lib.platforms.all;
-            extra_pkg_config = {
-                # allowUnfree = true;
-            };
-            dependencyOverlays = [
-                # use `pkgs.neovimPlugins`, which is a set of our plugins.
-                (utils.standardPluginOverlay inputs)
-                # add any other flake overlays here.
-            ];
+	# see :help nixCats.flake.outputs
+	outputs = {
+		# self,
+		nixpkgs,
+		nixCats,
+		...
+	} @ inputs: let
+		inherit (nixCats) utils;
+		luaPath = ./.; # lua configuration is in this same directory
+		forEachSystem = utils.eachSystem nixpkgs.lib.platforms.all;
+		extra_pkg_config = {
+			# allowUnfree = true;
+		};
+		dependencyOverlays = [
+			# use `pkgs.neovimPlugins`, which is a set of our plugins.
+			(utils.standardPluginOverlay inputs)
+			# add any other flake overlays here.
+		];
 
-            categoryDefinitions = import ./cats.nix inputs;
-            packageDefinitions = import ./nvims.nix inputs;
-            defaultPackageName = "nixLoki";
-        in
-        # see :help nixCats.flake.outputs.exports
-        forEachSystem (
-            system:
-            let
-                # the builder function that makes it all work
-                nixCatsBuilder = utils.baseBuilder luaPath {
-                    inherit
-                        nixpkgs
-                        system
-                        dependencyOverlays
-                        extra_pkg_config
-                        ;
-                } categoryDefinitions packageDefinitions;
-                defaultPackage = nixCatsBuilder defaultPackageName;
-                # this is just for using utils such as pkgs.mkShell
-                # The one used to build neovim is resolved inside the builder
-                # and is passed to our categoryDefinitions and packageDefinitions
-                # pkgs = import nixpkgs { inherit system; };
-            in
-            {
-                # these outputs will be wrapped with ${system} by utils.eachSystem
+		categoryDefinitions = import ./cats.nix inputs;
+		packageDefinitions = import ./nvims.nix inputs;
+		defaultPackageName = "nixLoki";
+	in
+		# see :help nixCats.flake.outputs.exports
+		forEachSystem (
+			system: let
+				# the builder function that makes it all work
+				nixCatsBuilder =
+					utils.baseBuilder luaPath {
+						inherit
+							nixpkgs
+							system
+							dependencyOverlays
+							extra_pkg_config
+							;
+					}
+					categoryDefinitions
+					packageDefinitions;
+				defaultPackage = nixCatsBuilder defaultPackageName;
+				# this is just for using utils such as pkgs.mkShell
+				# The one used to build neovim is resolved inside the builder
+				# and is passed to our categoryDefinitions and packageDefinitions
+				# pkgs = import nixpkgs { inherit system; };
+			in {
+				# these outputs will be wrapped with ${system} by utils.eachSystem
 
-                # this will make a package out of each of the packageDefinitions defined above
-                # and set the default package to the one passed in here.
-                packages = utils.mkAllWithDefault defaultPackage;
+				# this will make a package out of each of the packageDefinitions defined above
+				# and set the default package to the one passed in here.
+				packages = utils.mkAllWithDefault defaultPackage;
 
-                # choose your package for devShell
-                # and add whatever else you want in it.
-                # devShells = {
-                #   default = pkgs.mkShell {
-                #     name = defaultPackageName;
-                #     packages = [ defaultPackage ];
-                #     inputsFrom = [ ];
-                #     shellHook = ''
-                #     '';
-                #   };
-                # };
+				# choose your package for devShell
+				# and add whatever else you want in it.
+				# devShells = {
+				#   default = pkgs.mkShell {
+				#     name = defaultPackageName;
+				#     packages = [ defaultPackage ];
+				#     inputsFrom = [ ];
+				#     shellHook = ''
+				#     '';
+				#   };
+				# };
+			}
+		)
+		// (
+			let
+				# we also export a nixos module to allow reconfiguration from configuration.nix
+				nixosModule =
+					utils.mkNixosModules {
+						moduleNamespace = [defaultPackageName];
+						inherit
+							defaultPackageName
+							dependencyOverlays
+							luaPath
+							categoryDefinitions
+							packageDefinitions
+							extra_pkg_config
+							nixpkgs
+							;
+					};
+				# and the same for home manager
+				homeModule =
+					utils.mkHomeModules {
+						moduleNamespace = [defaultPackageName];
+						inherit
+							defaultPackageName
+							dependencyOverlays
+							luaPath
+							categoryDefinitions
+							packageDefinitions
+							extra_pkg_config
+							nixpkgs
+							;
+					};
+			in {
+				# these outputs will be NOT wrapped with ${system}
 
-            }
-        )
-        // (
-            let
-                # we also export a nixos module to allow reconfiguration from configuration.nix
-                nixosModule = utils.mkNixosModules {
-                    moduleNamespace = [ defaultPackageName ];
-                    inherit
-                        defaultPackageName
-                        dependencyOverlays
-                        luaPath
-                        categoryDefinitions
-                        packageDefinitions
-                        extra_pkg_config
-                        nixpkgs
-                        ;
-                };
-                # and the same for home manager
-                homeModule = utils.mkHomeModules {
-                    moduleNamespace = [ defaultPackageName ];
-                    inherit
-                        defaultPackageName
-                        dependencyOverlays
-                        luaPath
-                        categoryDefinitions
-                        packageDefinitions
-                        extra_pkg_config
-                        nixpkgs
-                        ;
-                };
-            in
-            {
+				# this will make an overlay out of each of the packageDefinitions defined above
+				# and set the default overlay to the one named here.
+				overlays =
+					utils.makeOverlays luaPath {
+						inherit nixpkgs dependencyOverlays extra_pkg_config;
+					}
+					categoryDefinitions
+					packageDefinitions
+					defaultPackageName;
 
-                # these outputs will be NOT wrapped with ${system}
+				nixosModules.default = nixosModule;
+				homeModules.default = homeModule;
 
-                # this will make an overlay out of each of the packageDefinitions defined above
-                # and set the default overlay to the one named here.
-                overlays = utils.makeOverlays luaPath {
-                    inherit nixpkgs dependencyOverlays extra_pkg_config;
-                } categoryDefinitions packageDefinitions defaultPackageName;
-
-                nixosModules.default = nixosModule;
-                homeModules.default = homeModule;
-
-                inherit utils nixosModule homeModule;
-                inherit (utils) templates;
-            }
-        );
+				inherit utils nixosModule homeModule;
+				inherit (utils) templates;
+			}
+		);
 }
--- nvims.nix
+++ nvims.nix
@@ -1,124 +1,122 @@
-inputs:
-let
-    inherit (inputs.nixCats) utils;
-    # package settings shared between loki nvim packages
-    loki_settings =
-        { ... }: # @misc
-        {
-            # unwrappedCfgPath = utils.mkLuaInline "(os.getenv('XDG_CONFIG_HOME') or '/home/james/.config') .. '/loki'";
-            wrapRc = true;
-            # whether runtime dependencies will overwrite existing dependencies or the inverse
-            # appends runtime dependencies to the end of the PATH not the start
-            suffix-path = true;
-            # appends sharedLibraries to the end of the LD_LIBRARY_PATH not the start
-            suffix-LD = true;
-            # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
-            hosts.python3.enable = true;
-            # hosts.node.enable = true;
-            # groups treesitter into the same directory
-            collate-grammars = true;
-            # neovim-unwrapped = null; pin a specific nvim version
-            # nvimSRC = null; # causes it to build from source
-        };
-    # enabled categories shared between loki nvim packages
-    # All categories included must be marked true, but false may be omitted.
-    # This entire set is also passed to nixCats for querying within the lua.
-    # see :help nixCats.flake.outputs.packageDefinitions
-    loki_cats =
-        { ... }: # @misc
-        {
-            general = true;
-            # plugins
-            lualine.lualine = true;
-            lualine.harpoon = true;
-            lualine.gitsigns = true;
-            catppuccin = true;
-            treesitter = true;
-            lspconfig = true;
-            conform = true;
-            trouble = true;
-            cmp = true;
-            telescope = true;
-            mini-indentscope = true;
-            vim-surround = true;
-            transparent = true;
-            comment = true;
-            web-devicons = true;
-            netrw = true;
-            vim-illuminate = true;
-            ts-autotag = true;
-            harpoon = true;
-            snacks = true;
-            autopairs = true;
-            smart-splits = true;
-            toggleterm = true;
-            iron = true;
-            undotree = true;
-            vim-fugitive = true;
-            yanky = true;
+inputs: let
+	inherit (inputs.nixCats) utils;
+	# package settings shared between loki nvim packages
+	loki_settings = {...}:
+	# @misc
+	{
+		# unwrappedCfgPath = utils.mkLuaInline "(os.getenv('XDG_CONFIG_HOME') or '/home/james/.config') .. '/loki'";
+		wrapRc = true;
+		# whether runtime dependencies will overwrite existing dependencies or the inverse
+		# appends runtime dependencies to the end of the PATH not the start
+		suffix-path = true;
+		# appends sharedLibraries to the end of the LD_LIBRARY_PATH not the start
+		suffix-LD = true;
+		# neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+		hosts.python3.enable = true;
+		# hosts.node.enable = true;
+		# groups treesitter into the same directory
+		collate-grammars = true;
+		# neovim-unwrapped = null; pin a specific nvim version
+		# nvimSRC = null; # causes it to build from source
+	};
+	# enabled categories shared between loki nvim packages
+	# All categories included must be marked true, but false may be omitted.
+	# This entire set is also passed to nixCats for querying within the lua.
+	# see :help nixCats.flake.outputs.packageDefinitions
+	loki_cats = {...}:
+	# @misc
+	{
+		general = true;
+		# plugins
+		lualine.lualine = true;
+		lualine.harpoon = true;
+		lualine.gitsigns = true;
+		catppuccin = true;
+		treesitter = true;
+		lspconfig = true;
+		conform = true;
+		trouble = true;
+		cmp = true;
+		telescope = true;
+		mini-indentscope = true;
+		vim-surround = true;
+		transparent = true;
+		comment = true;
+		web-devicons = true;
+		netrw = true;
+		vim-illuminate = true;
+		ts-autotag = true;
+		harpoon = true;
+		snacks = true;
+		autopairs = true;
+		smart-splits = true;
+		toggleterm = true;
+		iron = true;
+		undotree = true;
+		vim-fugitive = true;
+		yanky = true;
 
-            # langs
-            lang.lua = true;
-            lang.nix = true;
-            lang.go = true;
-            lang.c = true;
-            lang.python = true;
-            lang.bash = true;
-            lang.jinja = true;
-            lang.css = true;
-            lang.html = true;
-            lang.toml = true;
-            lang.markdown = true;
-            lang.javascript = true;
-            lang.json = true;
-        };
-    # extra specifications shared between loki nvim packages
-    loki_extra =
-        { ... }: # @misc
-        {
-            theme = "catppuccin";
-        };
-in
-{
-    nixLoki =
-        {
-            # pkgs,
-            # name,
-            # mkPlugin,
-            ...
-        }@args:
-        {
-            # see :help nixCats.flake.outputs.settings
-            settings = loki_settings args // {
-                # aliases may not conflict with your other packages.
-                aliases = [
-                    "loki"
-                    "nvim"
-                ];
-                configDirName = "loki";
-                extraName = "nixLoki";
-            };
-            categories = loki_cats args // { };
-            # things to pass to nvim that aren't categories
-            extra = loki_extra args // { };
-        };
-    # an extra test package with normal lua reload for fast edits
-    # nix doesnt provide the config in this package, allowing free reign to edit it.
-    testNixLoki =
-        {
-            # pkgs,
-            # name,
-            # mkPlugin,
-            ...
-        }@args:
-        {
-            settings = loki_settings args // {
-                wrapRc = false;
-                unwrappedCfgPath = utils.mkLuaInline "(os.getenv('XDG_CONFIG_HOME') or '/home/james/.config') .. '/testLoki'";
-                configDirName = "testLoki";
-                extraName = "testNixLoki";
-            };
-            categories = loki_cats args // { };
-            extra = loki_extra args // { };
-        };
+		# langs
+		lang.lua = true;
+		lang.nix = true;
+		lang.go = true;
+		lang.c = true;
+		lang.python = true;
+		lang.bash = true;
+		lang.jinja = true;
+		lang.css = true;
+		lang.html = true;
+		lang.toml = true;
+		lang.markdown = true;
+		lang.javascript = true;
+		lang.json = true;
+	};
+	# extra specifications shared between loki nvim packages
+	loki_extra = {...}:
+	# @misc
+	{
+		theme = "catppuccin";
+	};
+in {
+	nixLoki = {
+		# pkgs,
+		# name,
+		# mkPlugin,
+		...
+	} @ args: {
+		# see :help nixCats.flake.outputs.settings
+		settings =
+			loki_settings args
+			// {
+				# aliases may not conflict with your other packages.
+				aliases = [
+					"loki"
+					"nvim"
+				];
+				configDirName = "loki";
+				extraName = "nixLoki";
+			};
+		categories = loki_cats args // {};
+		# things to pass to nvim that aren't categories
+		extra = loki_extra args // {};
+	};
+	# an extra test package with normal lua reload for fast edits
+	# nix doesnt provide the config in this package, allowing free reign to edit it.
+	testNixLoki = {
+		# pkgs,
+		# name,
+		# mkPlugin,
+		...
+	} @ args: {
+		settings =
+			loki_settings args
+			// {
+				wrapRc = false;
+				unwrappedCfgPath = utils.mkLuaInline "(os.getenv('XDG_CONFIG_HOME') or '/home/james/.config') .. '/testLoki'";
+				configDirName = "testLoki";
+				extraName = "testNixLoki";
+			};
+		categories = loki_cats args // {};
+		extra = loki_extra args // {};
+	};
 }
